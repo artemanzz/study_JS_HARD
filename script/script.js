5955401bdcbfc398961d52e525eb5cb88430d389
Artem Anzin <artyomqaff@gmail.com>
2021-02-05 23:12:13 +0300

    <fix>: fixed mistakes

--- script/script.js
+++ script/script.js
@@ -1,14 +1,20 @@
 'use strict';
 
-let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-let date = new Date(2021, 1, 5);
+let week = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+let date = new Date();
 
-for (let i = 0; i < week.length; i++) {
-  if (i === (date.getDay() - 1)) {
+for (let i = 1; i < week.length; i++) {
+  if (i === (date.getDay())) {
     document.write('<div><b>' + week[i] + '</b></div>');
   } else if (i >= 5) {
     document.write('<div><i>' + week[i] + '</i></div>');
   } else {
     document.write('<div>' + week[i] + '</div>');
   }
+}
+
+if (date.getDay() === 0) {
+  document.write('<div><b>' + week[0] + '</b></div>');
+} else {
+  document.write('<div><i>' + week[0] + '</i></div>');
 }
